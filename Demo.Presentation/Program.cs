d81c4915434ff6a6d789eca88d7c64901b92392c
x-Muha <muhmdusam4@gmail.com>
2025-05-04 02:18:07 +0200

    Add project files.

---
+++ Demo.Presentation/Program.cs
@@ -0,0 +1,35 @@
+namespace Demo.Presentation
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // Check if all requests are Secure
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
---
+++ Demo.Presentation/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Demo.Presentation.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
