fa8c91af392f92d2d61bbbec3e6448d7e07a860f
x-Muha <muhmdusam4@gmail.com>
2025-05-17 18:12:08 +0200

    Part 07 Employee Module - Edit Action

--- Demo.Presentation/Controllers/EmployeesController.cs
+++ Demo.Presentation/Controllers/EmployeesController.cs
@@ -1,5 +1,7 @@
 ﻿using Demo.BusinessLogic.DataTransferObjects.EmployeeDTOs;
 using Demo.BusinessLogic.Services.Interfaces;
+using Demo.DataAccess.Models.EmployeeModel;
+using Demo.DataAccess.Models.Shared.Enums;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Demo.Presentation.Controllers
@@ -42,5 +44,50 @@ namespace Demo.Presentation.Controllers
             var employee = _employeeService.GetEmployeeDetails(id.Value);
             return employee is null? NotFound() : View(employee);
         }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (!id.HasValue) return BadRequest();
+            var employee = _employeeService.GetEmployeeDetails(id.Value);
+            if(employee is null) return NotFound();
+            var employeeDTO = new UpdatedEmployeeDTO()
+            {
+                Id = employee.Id,
+                Name = employee.Name,
+                Salary = employee.Salary,
+                Address = employee.Address,
+                Age = employee.Age,
+                Email = employee.Email,
+                PhoneNumber = employee.PhoneNumber,
+                IsActive = employee.IsActive,
+                HiringDate = employee.HiringDate,
+                Gender = Enum.Parse<Gender>(employee.Gender),
+                EmployeeType = Enum.Parse<EmployeeType>(employee.EmployeeType)
+            };
+            return View(employeeDTO);
+        }
+        [HttpPost]
+        public IActionResult Edit([FromRoute]int? id, UpdatedEmployeeDTO employeeDTO)
+        {
+            if(!id.HasValue || id.Value != employeeDTO.Id) return BadRequest();
+            if (!ModelState.IsValid) return View(employeeDTO);
+            try
+            {
+                var Result = _employeeService.UpdateEmployee(employeeDTO);
+                if (Result > 0) return RedirectToAction(nameof(Index));
+                ModelState.AddModelError(string.Empty, "Emp Not Updated");  
+            }
+            catch (Exception e)
+            {
+                if (environment.IsDevelopment())
+                    ModelState.AddModelError(string.Empty, e.Message);
+                else Logger.LogError(e.Message);
+                return View("ErrorView", e);
+            }
+            return View(employeeDTO);
+            
+        }
+
     }
 }
