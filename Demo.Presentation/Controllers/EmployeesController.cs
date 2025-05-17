4b5c0026f1a33eff8d2cf4d9060762dfd5faa320
x-Muha <muhmdusam4@gmail.com>
2025-05-17 15:49:41 +0200

    Part 05 Employee Module - Create Action

--- Demo.Presentation/Controllers/EmployeesController.cs
+++ Demo.Presentation/Controllers/EmployeesController.cs
@@ -1,14 +1,38 @@
-﻿using Demo.BusinessLogic.Services.Interfaces;
+﻿using Demo.BusinessLogic.DataTransferObjects.EmployeeDTOs;
+using Demo.BusinessLogic.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Demo.Presentation.Controllers
 {
-    public class EmployeesController(IEmployeeService _employeeService) : Controller
+    public class EmployeesController(IEmployeeService _employeeService, 
+        IWebHostEnvironment environment, ILogger<EmployeesController> Logger) : Controller
     {
         public IActionResult Index()
         {
             var Employees = _employeeService.GetAllEmployees();
             return View(Employees);
+        }
+        [HttpGet]
+        public IActionResult Create() => View();
+        [HttpPost]
+        public IActionResult Create(CreatedEmployeeDTO employeeDTO)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    int Result = _employeeService.AddEmployee(employeeDTO);
+                    if (Result > 0) return RedirectToAction("Index"); //Added
+                    else ModelState.AddModelError(string.Empty, "Not Added");
+                }
+                catch (Exception e)
+                {
+                    if(environment.IsDevelopment())
+                        ModelState.AddModelError(string.Empty,e.Message);
+                    else Logger.LogError(e.Message);
+                }
+            }
+            return View(employeeDTO);
         }
     }
 }
